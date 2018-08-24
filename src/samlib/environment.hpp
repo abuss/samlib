5bc3ad7ac73377f42b836e8fd7f5abd1e036ad85
Antal Buss <antal.buss@gmail.com>
2018-08-23 23:46:25 -0600

    Added CRTP to fix agents that interact with the environment

--- src/samlib/environment.hpp
+++ src/samlib/environment.hpp
@@ -8,22 +8,23 @@ namespace samlib
 struct empty_state;
 
 
+template<typename Env>
 struct environment
 {
 
   bool terminate = false;
 
   template <typename... Args, typename Fn>
-  agent<environment, empty_state, Args...> make_agent(Fn&& fn)
+  samlib::agent<Env, empty_state, Args...> make_agent(Fn&& fn)
   {
-    return agent<environment, empty_state, Args...>(*this, fn);
+    return agent<Env, empty_state, Args...>(*static_cast<Env*>(this), fn);
   }
 
   // Make an agent with local state
   template <typename... Args, typename LS, typename Fn>
-  agent<environment, LS, Args...> make_agent(LS ls, Fn&& fn)
+  samlib::agent<Env, LS, Args...> make_agent(LS ls, Fn&& fn)
   {
-    return agent<environment, LS, Args...>(*this, ls, fn);
+    return agent<Env, LS, Args...>(*static_cast<Env*>(this), ls, fn);
   }
 
 };
